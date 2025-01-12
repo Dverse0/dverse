769877dfa854db360b97720c552177b597d4b192
Dverse0 <dverrse@gmail.com>
2025-01-12 20:52:51 +0530

    Update about.js

--- about.js
+++ about.js
@@ -5,3 +5,13 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector('.container');
+    const colors = ['#f9f4f0', '#fff8e1', '#f7ebe1'];
+    let currentColorIndex = 0;
+
+    setInterval(() => {
+        currentColorIndex = (currentColorIndex + 1) % colors.length;
+        container.style.backgroundColor = colors[currentColorIndex];
+    }, 3000);
+});
